2d45921a33a6a3426b97ab489808ea4fd1419ea8
Jean-Christophe Melikian <jean.melikian@gmail.com>
2020-07-24 16:44:47 +0200

    lib: Add a NetworkModule serving Retrofit

---
+++ open-weather-map-client/src/main/java/fr/ozoneprojects/open_weather_map_client/internal/core/NetworkModule.kt
@@ -0,0 +1,38 @@
+package fr.ozoneprojects.open_weather_map_client.internal.core
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import fr.ozoneprojects.open_weather_map_client.internal.OpenWeatherMapApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+internal object NetworkModule {
+
+    private fun createHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .apply {
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        }.build()
+
+    private fun createMoshiConverterFactory(): MoshiConverterFactory =
+        MoshiConverterFactory.create(
+            Moshi.Builder().apply {
+                add(KotlinJsonAdapterFactory())
+            }.build()
+        ).withNullSerialization()
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .apply {
+
+                baseUrl(OpenWeatherMapApi.BASE_URL)
+                addConverterFactory(
+                    createMoshiConverterFactory()
+                )
+                client(createHttpClient())
+            }.build()
+    }
+}
