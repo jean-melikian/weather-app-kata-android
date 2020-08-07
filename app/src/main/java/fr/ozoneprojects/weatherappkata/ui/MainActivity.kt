d83f4e925b4c64af89480d0633beb35b6e1bdda2
Jean-Christophe Melikian <jean.melikian@gmail.com>
2020-08-07 11:58:22 +0200

    app: Move MainActivity in ui package

--- app/src/main/java/fr/ozoneprojects/weatherappkata/ui/MainActivity.kt
+++ app/src/main/java/fr/ozoneprojects/weatherappkata/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package fr.ozoneprojects.weatherappkata
+package fr.ozoneprojects.weatherappkata.ui
 
 import android.content.Context
 import android.content.SharedPreferences
@@ -8,6 +8,8 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.net.PlacesClient
+import fr.ozoneprojects.weatherappkata.BuildConfig
+import fr.ozoneprojects.weatherappkata.R
 import fr.ozoneprojects.weatherappkata.dataadapter.LocationsRepositoryImpl
 import fr.ozoneprojects.weatherappkata.ui.locations.LocationsFragment
 import fr.ozoneprojects.weatherappkata.ui.locations.LocationsViewModel
